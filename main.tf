6e055b4a243a291720e8ba5a93b32e99f0ce3750
Digbijayinee Clara <52991229+DigbijayineeClara07@users.noreply.github.com>
2024-10-22 10:02:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "bucket" {
   source  = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
   version = "~> 6.0"
 
-  name       = "clara-gcp-bucket"
+  name       = "clara-gcp-bucket-test2210"
   project_id = "stackguardian-nonprod"
   location   = "us"
 
