bf4d51a1e2216e1705a2a94084805118b27ac33b
Digbijayinee Clara <52991229+DigbijayineeClara07@users.noreply.github.com>
2024-10-30 17:42:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,24 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"  # Specify a version if needed
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"  # Specify a version as needed
+    }
+  }
+ backend "s3" {
+  region = "eu-central-1"
+ }
+}
+
 module "bucket" {
   source  = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
   version = "~> 6.0"
 
-  name       = "clara-gcp-bucket-test2u57210"
+  name       = "clara-gcp-bucket-test2u5hf7210"
   project_id = "stackguardian-nonprod"
   location   = "us"
 
